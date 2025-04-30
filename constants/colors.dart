eaa2ca6927dd4d21a5f1e1aaabc4db57d2e33997
georgewin18 <georgewk1809@gmail.com>
2025-04-30 11:22:22 +0700

    colors constants

---
+++ constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary  = Color(0xFF7E1AD1);
+  static const Color secondary = Color(0xFFBF8DE8);
+  static const Color textSecondary = Color(0xFF8C8C8C);
+  static const Color success = Color(0xFF38D300);
+  static const Color warning = Color(0xFFF2B900);
+  static const Color error = Color(0xFFFF5454);
+}

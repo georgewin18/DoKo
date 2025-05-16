4ceaf57558ec43ddcf7fc748d4477c47532b524b
rifkialaudin <rifkialaudin5@gmail.com>
2025-05-16 11:11:20 +0700

    Add Focus Mode Page

---
+++ lib/pages/focus_mode_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FocusModePage extends StatefulWidget {
+  const FocusModePage({super.key});
+
+  @override
+  State<FocusModePage> createState() => _FocusModePageState();
+}
+
+class _FocusModePageState extends State<FocusModePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

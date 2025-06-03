f45734907e2bd4f1396a83769d98da78ed3842c9
Kansaadeneva <155238017+Kansaadeneva@users.noreply.github.com>
2025-06-04 01:19:59 +0700

    tambah edit dan delete focusmode

--- lib/components/delete_confirmation_dialog.dart
+++ lib/components/delete_confirmation_dialog.dart
@@ -2,6 +2,8 @@ import 'package:app/db/task_group_db_helper.dart';
 import 'package:app/models/task_group_model.dart';
 import 'package:app/models/task_model.dart';
 import 'package:flutter/material.dart';
+import 'package:app/models/focus_timer_model.dart';
+import 'package:app/db/focus_timer_db_helper.dart';
 
 Future<bool> deleteTaskConfirmationDialog(
   BuildContext context,
@@ -163,6 +165,99 @@ Future<bool> deleteGroupConfirmationDialog(
                           context,
                           true,
                         ); // Kembali ke TaskGroupPage, trigger refresh
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFFF5454),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: const Text(
+                        'Yes',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+
+  return result ?? false;
+}
+
+Future<bool> deleteFocusTimerConfirmationDialog(
+  BuildContext context,
+  FocusTimer timer,
+) async {
+  final result = await showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: SizedBox(
+          width: 349,
+          height: 166,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  'Are you sure you want to delete "${timer.name}" ?',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 130,
+                    height: 40,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF7E1AD1),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      child: const Text(
+                        'No',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 130,
+                    height: 40,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await FocusTimerDbHelper.deleteFocusTimer(timer.id);
+                        Navigator.pop(context, true);
                       },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: const Color(0xFFFF5454),

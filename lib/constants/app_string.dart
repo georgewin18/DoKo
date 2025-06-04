dc6b01cb32701c304cfa992279e52847c484e83b
NAZIBURUK <145909588+Zorgons905@users.noreply.github.com>
2025-06-04 21:20:13 +0700

    tambahan file untuk constants

--- lib/constants/app_string.dart
+++ lib/constants/app_string.dart
@@ -1,4 +1,70 @@
-// ISI SEMUA HAL BERKAITAN DENGAN JARGON,
-// NAMA APP DISINI
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'user_preferences.dart';
 
-const title = 'Doko';
+class AppString {
+  final bool isEn;
+
+  AppString(BuildContext context)
+    : isEn =
+          Provider.of<UserPreferences>(context, listen: false).language == 'en';
+
+  //
+  String get greeting => isEn ? 'Hello!' : 'Halo!';
+  String get jargon =>
+      isEn ? 'Manage your task with Doko' : 'Atur tugasmu dengan Doko';
+  //
+  String get name => isEn ? 'Name' : 'Nama';
+  String get date => isEn ? 'Date' : 'Tanggal';
+  //
+  String get taskGroupTitle => isEn ? 'Task Group' : 'Grup Tugas';
+  String get taskTitle => isEn ? 'Task Title' : 'Judul Tugas';
+  String get taskListTitle => isEn ? 'Task List' : 'List Tugas';
+  String get myTask => isEn ? "My Task" : "Tugas Saya";
+
+  String get deadline => isEn ? "Deadline" : "Tenggat";
+  String get addNotesHint => isEn ? "Add Notes..." : "Tambah Catatan...";
+  String get addYourLinkHint =>
+      isEn ? "Add your link here..." : "Tautkan link anda di sini...";
+  String get cancel => isEn ? "Cancel" : "Batalkan";
+  String get progress => isEn ? "Progress" : "Kemajuan";
+  //
+  String get editGroupTitle => isEn ? "Edit Info Group" : "Edit Info Grup";
+  String get deleteGroupTitle => isEn ? "Delete Group" : "Hapus Grup";
+  String get sortBy => isEn ? "Sort By" : "Urutkan berdasarkan";
+  //
+  String get emptyTaskNotifier =>
+      isEn
+          ? "You don't have any task for now!"
+          : "Anda tidak punya tugas untuk saat ini!";
+  String get emptyTitleNotifier =>
+      isEn ? "Title can't be empty" : "Judul tak boleh kosong";
+  String get taskAddedNotifier =>
+      isEn ? "Task successfully added" : "Tugas berhasil ditambahkan";
+  String get noTaskNotifier =>
+      isEn ? "There is no task on" : "Tidak ada tugas pada";
+  //
+  String get groupTitleHint => isEn ? "Group Title" : "Judul Grup";
+  String get descriptionHint => isEn ? "Description..." : "Deskripsi";
+  String get saveTask => isEn ? "Save" : "Simpan";
+  String get taskDeleted => isEn ? "Task deleted" : "Tugas terhapus";
+  String get taskUpdated => isEn ? "Task updated" : "Tugas terupdate";
+  String get save => isEn ? "Save" : "Simpan";
+  //
+  String get unknownGroup => isEn ? "Unknown Group" : "Grup tak dikenali";
+  String get createGroup => isEn ? "Create New Group" : "Buat Grup Baru";
+  String get noGroupNotifier =>
+      isEn
+          ? "You don't have any group, create one now!"
+          : "Anda tidak punya grup, buatlah sekarang!";
+
+  String get uniqueTitleNotifier =>
+      isEn
+          ? "Title must be unique!\nYou already have this group"
+          : "Judul harus unik!\n Anda sudah mempunyai grup ini!";
+  String get failedUpdatingTaskNotifier =>
+      isEn ? "Failed updating task" : "Gagal memperbarui tugas";
+
+  String get selectDeadlineNotifier =>
+      isEn ? "Please select a deadline" : "Mohon pilih tenggat tugas";
+}
--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,40 +1,79 @@
 import 'package:flutter/material.dart';
 
-enum TaskPriority { normal, priority }
+/*
+untuk masalah color mengikuti preferensi user jika user
+memilih violet (defaultnya violet) maka ia (colorPreference) akan
+di set violet.
+*/
 
-enum TaskStatus {
-  overdue, // Red - statusColor[0]
-  ongoing, // Brown - statusColor[1]
-  notStarted, // Gray - statusColor[2]
-  dueToday, // Yellow - statusColor[3]
-  completed, // Green - statusColor[4]
-  event, // Blue - statusColor[5]
-  priority, // Purple - statusColor[6]
-}
+/*
+jangan gunakan list color ini secara literal 
+gunakan colorPreference dari user
+*/
 
-const themeColors = [
-  Color(0xffFF5555),
+const List<Color> violet = [
+  Color(0xff30074f), //0 untuk gradasi gelap navbar
+  Color(0xff4d107f), //1 untuk navbar dan counter di group
+  Color(0xff7E1AD1), //2 untuk appbar, calendar, dan judul group
+  Color(0xffb877f0), //3 untuk gradasi appbar, dan warna description
+];
+
+const List<Color> grass = [
+  Color(0xff236e0e),
+  Color(0xff1c8720),
+  Color(0xff35a017),
+  Color(0xFF6BEA66),
+];
+
+const List<Color> rose = [
+  Color(0xffff0000),
+  Color(0xffff5555),
+  Color(0xffff8080),
+  Color(0xffffb3b3),
+];
+
+const List<Color> ocean = [
+  Color(0xff2851c6),
+  Color(0xff3f72e0),
+  Color(0xff0f9cf8),
+  Color(0xff63cbff),
+];
+
+const List<Color> amber = [
+  Color(0xfffa7d00),
+  Color(0xffffb333),
+  Color(0xffffd34d),
+  Color(0xffffee99),
+];
+
+const List<Color> wood = [
   Color(0xff663300),
+  Color(0xff8a4a2d),
+  Color(0xffaf6e4d),
+  Color(0xffdfa57d),
+];
+
+const List<Color> metal = [
   Color(0xff404040),
-  Color(0xffFA7D00),
-  Color(0xff199328),
-  Color(0xff2851C6),
-  Color(0xff7E1AD1),
+  Color(0xff5e5e5e),
+  Color(0xff757575),
+  Color(0xff9c9c9c),
 ];
 
-const statusColor = [
-  Color(0xffD60000),
-  Color(0xffAF6E4D),
-  Color(0xFF9C9C9C),
-  Color(0xffFFC000),
-  Color(0xff2BE13C),
-  Color(0xFF26B5FD),
-  Color(0xffBE28BE),
-];
+/*
+Ini untuk warna button sesuai pesan 
+yang ingin disampaikan
+*/
+const Color danger = Color(0xffDC3545);
+const Color warning = Color(0xffFFC107);
+const Color success = Color(0xff2ECC71);
 
-Color white = Color(0xFFFFFFFF); //buat foreground / tulisan
-Color gray1 = Color(0xFFC0C0C0); //buat hint text
-Color gray2 = Color(0xFF7F7F7F); //buat box shadow
-Color gray3 = Color(0xFF3F3F3F); //buat title
-Color black = Color(0xFF000000); //buat tulisan
-Color transparent = Colors.transparent;
+/*
+Ini default pallete untuk lightmode dan darkmode
+*/
+const Color white = Color(0xFFFFFFFF); //buat foreground / tulisan
+const Color gray1 = Color(0xFFC0C0C0); //buat hint text
+const Color gray2 = Color(0xFF7F7F7F); //buat box shadow
+const Color gray3 = Color(0xFF3F3F3F); //buat title
+const Color black = Color(0xFF000000); //buat tulisan
+const Color transparent = Colors.transparent;
